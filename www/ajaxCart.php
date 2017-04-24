816e9257cf8a337b49448de170c3839eb769c95e
MSITE.TOP <admin@msite.top>
2017-04-24 12:42:21 +0500

    карточка

---
+++ www/ajaxCart.php
@@ -0,0 +1,10 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
+			<?$APPLICATION->IncludeComponent("bitrix:sale.basket.basket.small", "smal_basket2", Array(
+				"PATH_TO_BASKET" => "/basket/index.php",	// Страница корзины
+				"PATH_TO_ORDER" => "/personal/order.php",	// Страница оформления заказа
+				"SHOW_DELAY" => "N",	// Показывать отложенные товары
+				"SHOW_NOTAVAIL" => "N",	// Показывать товары, недоступные для покупки
+				"SHOW_SUBSCRIBE" => "N",	// Показывать товары, на которые подписан покупатель
+				),
+				false
+			);?>
